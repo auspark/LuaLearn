70c6eb132afcd44a48c106c5556e9dfdc65e6b2a
Jerry.Yang <yingxi_yang@trantest.com>
2020-01-13 17:04:40 +0800

    循序遍历

--- pairs.lua
+++ pairs.lua
@@ -17,3 +17,30 @@ pairs
 每个元素会且只会出现1次
 
 ]]
+
+
+t = {10, print, 12, 'hi'}
+for k,v in ipairs(t) do
+	print(k,v)
+end
+
+-- 1	10
+-- 2	function: 0x7fcc70c04180
+-- 3	12
+-- 4	hi
+--[[
+ipairs: Lua会确保遍历是按照顺序进行的
+]]
+
+
+for i=1,#t do
+	print(i,t[i])
+end
+-- 数值型for循环，也能保证是按照循序进行的
+
+
+
+
+
+
+
