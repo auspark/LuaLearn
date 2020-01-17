d25f468227f4de4b493648ef402d27b38831cf89
Jerry.Yang <yingxi_yang@trantest.com>
2020-01-17 16:11:54 +0800

    使用coroutine.wrap()

--- Arrange.lua
+++ Arrange.lua
@@ -24,12 +24,28 @@ function permutations( a )
 	end
 end
 
+--
+-- 函数permutations使用了Lua语言中一种常见模式，就是将唤醒对应协程的调用包装在一个函数中
+-- 由于这种模式比较常见，所以Lua语言专门提供了一个特殊函数coroutine.wrap()来完成这个功能
+--
+-- coroutine.wrap()与create()的比较
+-- 相同点：函数wrap也用来创建一个新的协程。
+-- 不同点：1、函数wrap返回的不是协程本身而是一个函数，当这个函数被调用时会唤醒协程。
+--        2、该函数的第一个返回值不是错误代码，当遇到错误时该函数会抛出异常
+--
+function permutations_wrap( a )
+	return coroutine.wrap(function() ArrangeArr(a)	end)
+end
+
+
+
 -- local array = {1,2}
-local array = {'a',2,'d'}
+local array = {1,2,3}
+-- local array = {'a','b','c'}
 -- local array = {1,2,3,4}
 -- local array = {1,2,3,4,5}
 -- ArrangeArr(array)
-for p in permutations(array) do -- 迭代获取每一种排列, 每次迭代运行一次resume，到yield停住并返回yield的值
+for p in permutations_wrap(array) do -- 迭代获取每一种排列, 每次迭代运行一次resume，到yield停住并返回yield的值
 	for i=1,#p do
 		io.write(p[i]," ") -- 排列是数组，使用循环打印出来
 	end
